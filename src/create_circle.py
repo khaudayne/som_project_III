e11df9631bbc10fbd2acce205628a1d2a2146687
NguyenDinhKhauNe <nguyendinhkhai158@gmail.com>
2024-11-25 00:02:02 +0700

    SOM region vs 1.1 Test new map

--- src/create_circle.py
+++ src/create_circle.py
@@ -1,10 +1,10 @@
 import random
 MIN_POS = 20
 MAX_POS = 980
-MAX_RADIUS = 15
-MIN_RADIUS = 5
+MAX_RADIUS = 35
+MIN_RADIUS = 20
 MAX_SIZE = 1000
-MAX_REWARD = 20
+MAX_REWARD = 10
 MIN_REWARD = 1
 filename = "assets/circles.txt"
 with open(filename, "w") as file:
@@ -12,5 +12,5 @@ with open(filename, "w") as file:
         x = round(random.uniform(MIN_POS, MAX_POS), 4)
         y = round(random.uniform(MIN_POS, MAX_POS), 4)
         r = round(random.uniform(MIN_RADIUS, MAX_RADIUS), 4)
-        reward = random.randint(MIN_RADIUS, MAX_REWARD)
+        reward = random.randint(MIN_REWARD, MAX_REWARD)
         file.write(f"{_ + 1} {x} {y} {r} {reward}\n")
