1220fb786d8adf19954f02fad13a07cb01489ddd
NguyenDinhKhauNe <nguyendinhkhai158@gmail.com>
2024-12-10 01:50:08 +0700

    add fig compare

--- src/test.py
+++ src/test.py
@@ -1,87 +1,60 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# N labels of N number of values in the array
-xlabels_som = ['SOM', 'SOM with greedy']
-bar_colors = ['#8A9A5B', '#F6AC69']
-plot_color_1 = "#355E3B"
-plot_color_2 = "#F57C73"
+x = [20, 30, 40, 50]
+sp = ("2 robots", "3 robots", "4 robots", "5 robots")
+list_cost_after = [
+    [175.0, 58.0],
+    [118.0, 70.0, 45.0],
+    [80.0, 60.0, 56.0, 37.0],
+    [63.0, 55.0, 56.0, 37.0, 22.0]
+]
+list_cost_before = [
+    [172.0, 61.0],
+    [93.0, 62.0, 78.0],
+    [39.0, 62.0, 78.0, 54.0],
+    [41.0, 61.0, 58.0, 47.0, 26.0]
+]
+cost_all_robot_after = [
+    233.0,
+    233.0,
+    233.0,
+    233.0
+]
+cost_all_robot_before = [
+    233.0,
+    233.0,
+    233.0,
+    233.0
+]
 
-fig, axs = plt.subplots(1, 4, figsize=(12, 3))
-x_label = np.array([0.1, 0.2])
-x_label_plot_1 = np.array([0.05, 0.15])
-x_label_plot_2 = np.array([0.15, 0.25])
+# Tạo figure và trục đầu tiên
+fig, ax1 = plt.subplots()
 
-# Các trường thuộc tính dữ liệu
-m_1_max = 864.8166191017203
-m_1_min = 763.7544528987399
-m_1_avg = 815.8887811781401
-m_1_max_after = 843.657820644596768
-m_1_min_after = 822.991277393604832
-m_1_avg_after = 830.124313163504098
-axs[0].set_ylim(500, 1000)
+# Vẽ đồ thị đầu tiên (trục Y bên trái)
+for i in range(4):
+    start_x = x[i]
+    data_after = list_cost_after[i]
+    data_before = list_cost_before[i]
+    sz = len(data_after)
+    number_robot = sz * 2
+    offset_w = (number_robot - 1) / 2
+    x_tmp = np.arange(start_x, start_x + number_robot) - offset_w
+    for j in range(sz):
+        bar = ax1.bar(x_tmp[j], data_after[j], 1, color="orange", edgecolor= 'black')
+        if i == 0 and j == 0:
+            bar.set_label("reward after one robot")
+    for j in range(sz):
+        bar = ax1.bar(x_tmp[j + sz], data_before[j], 1, color="lime", edgecolor= 'black')
+        if i == 0 and j == 0:
+            bar.set_label("reward before one robot")
+ax1.plot(x, cost_all_robot_after, color='blue', label='reward after all robot')
+ax1.plot(x, cost_all_robot_before, color='green', label='reward before all robot')
 
-axs[0].bar(x_label ,np.array([m_1_max, m_1_max_after]) - np.array([m_1_min, m_1_min_after]), 0.05, bottom=np.array([m_1_min, m_1_min_after]), label=xlabels_som, color=bar_colors)
-axs[0].xaxis.set_visible(False)
-
-# Add some text for labels, title and custom x-axis tick labels, etc.
-axs[0].set_ylabel('Total reward')
-axs[0].set_xlim(0, 0.3)
-axs[0].set_title('30g_1r_lb')
-axs[0].legend()
-
-axs[0].plot(x_label_plot_1, np.array([m_1_avg, m_1_avg]), linestyle = 'dotted', color=plot_color_1)
-axs[0].plot(x_label_plot_2, np.array([m_1_avg_after, m_1_avg_after]), linestyle = 'dotted', color=plot_color_2)
-
-m_2_max = 4355.4220388651165
-m_2_min = 3794.013129081338
-m_2_avg = 4046.281555944204
-m_2_max_after = 4267.672927268394233
-m_2_min_after = 3772.871213629809972
-m_2_avg_after = 3897.587362954360287
-axs[1].set_ylim(3000, 5000)
-
-axs[1].bar(x_label ,np.array([m_2_max, m_2_max_after]) - np.array([m_2_min, m_2_min_after]), 0.05, bottom=np.array([m_2_min, m_2_min_after]), label=xlabels_som, color=bar_colors)
-axs[1].set_xlim(0, 0.3)
-axs[1].xaxis.set_visible(False)
-axs[1].set_title('30g_1r_hb')
-
-axs[1].plot(x_label_plot_1, np.array([m_2_avg, m_2_avg]), linestyle = 'dotted', color=plot_color_1)
-axs[1].plot(x_label_plot_2, np.array([m_2_avg_after, m_2_avg_after]), linestyle = 'dotted', color=plot_color_2)
-
-m_3_max = 2014.685419568923
-m_3_min = 1879.0569941221365
-m_3_avg = 1965.828786025737
-m_3_max_after = 2009.321502711029552
-m_3_min_after = 1929.209084853725926
-m_3_avg_after = 1989.447908853795525
-axs[2].set_ylim(1500, 2500)
-
-axs[2].bar(x_label ,np.array([m_3_max, m_3_max_after]) - np.array([m_3_min, m_3_min_after]), 0.05, bottom=np.array([m_3_min, m_3_min_after]), label=xlabels_som, color=bar_colors)
-axs[2].xaxis.set_visible(False)
-axs[2].set_xlim(0, 0.3)
-axs[2].set_title('150g_3r_lb')
-
-axs[2].plot(x_label_plot_1, np.array([m_3_avg, m_3_avg]), linestyle = 'dotted', color=plot_color_1)
-axs[2].plot(x_label_plot_2, np.array([m_3_avg_after, m_3_avg_after]), linestyle = 'dotted', color=plot_color_2)
-
-
-m_4_max = 9978.346141096552
-m_4_min = 9105.617138761208
-m_4_avg = 9584.2631815378
-m_4_max_after = 7649.574916449048033
-m_4_min_after = 7251.441291135427491
-m_4_avg_after = 7432.260968255885018
-axs[3].set_ylim(7000, 12000)
-
-axs[3].bar(x_label ,np.array([m_4_max, m_4_max_after]) - np.array([m_4_min, m_4_min_after]), 0.05, bottom=np.array([m_4_min, m_4_min_after]), label=xlabels_som, color=bar_colors)
-axs[3].xaxis.set_visible(False)
-axs[3].set_xlim(0, 0.3)
-axs[3].set_title('150g_3r_hb')
-
-axs[3].plot(x_label_plot_1, np.array([m_4_avg, m_4_avg]), linestyle = 'dotted', color=plot_color_1)
-axs[3].plot(x_label_plot_2, np.array([m_4_avg_after, m_4_avg_after]), linestyle = 'dotted', color=plot_color_2)
-
-
-fig.tight_layout()
+ax1.set_ylabel("Reward")  # Nhãn cho trục Y (bên trái)
+ax1.set_ylim(0,270)
+ax1.set_xlabel("Number of robots")
+ax1.set_xticks(x, sp)
+ax1.legend(loc='upper left', ncols = 2)
+plt.title("Case high budget in map this has 100 goals")
 plt.show()
---
+++ src/test2.py
@@ -0,0 +1,4 @@
+import numpy as np
+a = np.arange(20, 24)
+a = a - 1.5
+print(a)
